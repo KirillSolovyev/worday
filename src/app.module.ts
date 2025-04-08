753570e1c867942ceeb54253e84548a319159547
Kirill Solovyov <kirill.solovyev.98@gmail.com>
2025-04-08 21:49:15 +0500

    feat: add db provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { DBProvider } from '@/providers/db-provider';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { WordModule } from './features/word-module';
+import { UserModule } from './entities/user';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DBProvider, WordModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
