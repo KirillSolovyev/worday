5ef52ecb71f88e1a63b175846062588fd489b085
Kirill Solovyov <kirill.solovyev.98@gmail.com>
2025-04-18 17:03:34 +0500

    feat: add language and level enum

---
+++ src/shared/config/languages.ts
@@ -0,0 +1,15 @@
+export enum Language {
+  EN = 'en', // English
+  ES = 'es', // Spanish
+  ZH = 'zh', // Chinese
+  HI = 'hi', // Hindi
+  AR = 'ar', // Arabic
+  PT = 'pt', // Portuguese
+  BN = 'bn', // Bengali
+  FR = 'fr', // French
+  DE = 'de', // German
+  JA = 'ja', // Japanese
+  KK = 'kk', // Kazakh
+  RU = 'ru', // Russian
+  KO = 'ko', // Korean
+}
