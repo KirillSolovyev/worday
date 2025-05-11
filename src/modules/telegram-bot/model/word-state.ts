416e51da6e57270f222a6c8d4603bae694457867
KirillSolovyev <kirill.solovyev.98@gmail.com>
2025-05-11 18:47:14 +0500

    feat: handle the already generated word error

--- src/modules/telegram-bot/model/word-state.ts
+++ src/modules/telegram-bot/model/word-state.ts
@@ -3,7 +3,9 @@ import { Context } from 'telegraf';
 import { prettyTL } from '@/shared/lib/pretty-tl';
 import { UserStateEnum } from '@/entities/user-state';
 import { UserService } from '@/services/user-service';
-import { WordOfDayService } from '@/features/word-of-day-module';
+import { WordOfDayService, WordOfDayTimeLimitError } from '@/features/word-of-day-module';
+import type { User } from '@/entities/user';
+import type { Word } from '@/entities/word';
 
 import { AbstractState } from './abstract-state';
 import { TelegramBotCommand } from '../types';
@@ -26,25 +28,54 @@ export class WordState extends AbstractState {
   async handle() {
     try {
       this.logger.log('Handling word of the day');
+      const user = await this.getUser();
 
-      await this.ctx.reply('Generating word of the day...');
+      await this.generateWord(user);
+    } catch (error) {
+      this.logger.error('Failed to handle word of the day', error);
+      throw new Error('Oops, I could not generate the word of the day. Please try again');
+    }
+  }
 
-      const user = await this.getUser({ state: true, settings: true, words: true });
-      const word = await this.wordOfDayService.getWord(user);
-
-      const formattedExamples = word.examples.map(({ value }) => `- ${value}`).join('\n');
-      const formattedMessage = prettyTL(`
+  private buildMessage(word: Word) {
+    const formattedExamples = word.examples.map(({ value }) => `- ${value}`).join('\n');
+    const formattedMessage = prettyTL(`
         *${word.word}*
 
         _${word.definition}_
 
         ${formattedExamples}
       `);
+    return formattedMessage;
+  }
+
+  private async generateWord(user: User) {
+    try {
+      await this.ctx.reply('Generating word of the day...');
+
+      const word = await this.wordOfDayService.generateWord(user);
+      const formattedMessage = this.buildMessage(word);
 
       await this.ctx.reply(formattedMessage, { parse_mode: 'Markdown' });
     } catch (error) {
-      this.logger.error('Failed to handle word of the day', error);
-      throw new Error('Oops, I could not generate the word of the day. Please try again');
+      if (error instanceof WordOfDayTimeLimitError) {
+        await this.getLastWord(user);
+      } else {
+        throw error;
+      }
     }
+  }
+
+  private async getLastWord(user: User) {
+    const lastWord = await this.wordOfDayService.getLastWord(user);
+    const formattedMessage = this.buildMessage(lastWord);
+
+    const formattedErrorMsg = prettyTL(`
+      You have already received the word of the day today. Please try tomorrow
+      
+      As for now you can repeat the last word:
+      ${formattedMessage}
+    `);
+    await this.ctx.reply(formattedErrorMsg, { parse_mode: 'Markdown' });
   }
 }
